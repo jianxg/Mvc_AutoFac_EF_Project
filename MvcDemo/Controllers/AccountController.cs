212041321579168d28dcc3906b6a25585396c410
jianxg <419533848@qq.com>
2019-11-29 17:15:05 +0800

    Message:code commit

--- MvcDemo/Controllers/AccountController.cs
+++ MvcDemo/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using MvcDemo.DAL;
+using MvcDemo.ViewModels.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +16,7 @@ namespace MvcDemo.Controllers
         // GET: Account
         public ActionResult Index()
         {
-            return View();
+            return View(db.SysUser);
         }
 
         /// <summary>
@@ -79,6 +81,137 @@ namespace MvcDemo.Controllers
             }
             return password;
         }
+
+        /// <summary>
+        /// 更新对象数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EFUpdateDemo()
+        {
+            //第一步，找到对象
+            var sysUser = db.SysUser.FirstOrDefault(u => u.UserName == "Tom");
+            //更新对象数据
+            if (sysUser!=null)
+            {
+                sysUser.UserName = "TomCat";
+            }
+            //保存修改
+            db.SaveChanges();
+            return View();
+        }
+
+        /// <summary>
+        /// 新增一个实体对象
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EFAddDemo()
+        {
+            //创建一个新的实体对象
+            var newSysUser = new SysUser()
+            {
+                UserName = "Scott",
+                PassWord="tiger",
+                Email="Scott@sohu.com"
+            };
+            //增加
+            db.SysUser.Add(newSysUser);
+            db.SaveChanges();
+            return View();
+        }
+
+        /// <summary>
+        /// 删除一个实体对象
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EFDeleteDemo()
+        {
+            //查找实体对象
+            var sysUser = db.SysUser.FirstOrDefault(a => a.UserName == "Scott");
+            //删除
+            if (sysUser != null)
+            {
+                db.SysUser.Remove(sysUser);
+            }
+            db.SaveChanges();
+            return View();
+        }
+
+        /// <summary>
+        /// 查询用户及角色
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int id)
+        {
+            SysUser sysUser = db.SysUser.Find(id);
+            return View(sysUser);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 创建用户
+        /// </summary>
+        /// <param name="sysUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(SysUser sysUser)
+        {
+            db.SysUser.Add(sysUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 修改用户=--获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(int id)
+        {
+            SysUser sysUser = db.SysUser.Find(id);
+            return View(sysUser);
+        }
+
+        /// <summary>
+        /// 修改用户-修改
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Edit(SysUser sysUser)
+        {
+            db.Entry(sysUser).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            SysUser sysUser = db.SysUser.Find(id);
+            return View(sysUser);
+        }
+
+        /// <summary>
+        /// 确认删除用户
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SysUser sysUser = db.SysUser.Find(id);
+            db.SysUser.Remove(sysUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
 
     }
